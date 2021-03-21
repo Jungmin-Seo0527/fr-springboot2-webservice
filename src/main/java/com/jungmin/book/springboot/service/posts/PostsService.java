48d7afa06479ca98a936b43f2b5e97b83f728ea7
Jungmin-Seo0527 <soato1405@naver.com>
2021-03-21 19:57:23 +0900

    JPA 적용과 Entity 등록 기능, 테스트 코드

---
+++ src/main/java/com/jungmin/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.jungmin.book.springboot.service.posts;
+
+import com.jungmin.book.springboot.domain.posts.PostsRepository;
+import com.jungmin.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
